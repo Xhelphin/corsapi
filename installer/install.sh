fa75b0d03f3ff3420c7fa0a73d68c219cbc29c86
Xhelphin <jaxk.programmer@gmail.com>
2022-07-06 21:13:45 +0100

    Add uninstaller
    Also add messages to installer and uninstaller scripts

--- installer/install.sh
+++ installer/install.sh
@@ -10,3 +10,5 @@ mv ../controller /opt/corsapi/controller
 mv /opt/corsapi/controller/service/corsapi.service /etc/systemd/system/corsapi.service
 systemctl enable corsapi
 systemctl start corsapi
+
+echo "Done"
