ee8c0c2e2ed6b33aeb7449db456bebe2df6bc9e2
Xhelphin <jaxk.programmer@gmail.com>
2022-07-06 21:02:33 +0100

    only allow installer to run as root

--- installer/install.sh
+++ installer/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+fi
+
 mkdir /opt/corsapi/
 mv ../controller /opt/corsapi/controller
 mv /opt/corsapi/controller/service/corsapi.service /etc/systemd/system/corsapi.service
